dd6c5a9059dcf448c85fe26a002a3fd8835d6aba
AnupBudhathoki <kshetria656@gmail.com>
2026-03-27 13:28:07 +0545

    error fixing

--- PlayGrid/settings.py
+++ PlayGrid/settings.py
@@ -136,6 +136,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [BASE_DIR / 'static',
                     BASE_DIR / 'ttt' / 'static',]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
