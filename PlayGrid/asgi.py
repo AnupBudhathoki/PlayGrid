5299e4926dedf571e2b4c6ec661adcfb193d0112
AnupBudhathoki <kshetria656@gmail.com>
2026-03-28 10:12:04 +0545

    fix asgi env

--- PlayGrid/asgi.py
+++ PlayGrid/asgi.py
@@ -7,12 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PlayGrid.settings')
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import ttt.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PlayGrid.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
