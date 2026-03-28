eee67d8391dd5d7444c9cc548663f9e3cc89f632
AnupBudhathoki <kshetria656@gmail.com>
2026-03-28 10:25:50 +0545

    app loading fix

--- PlayGrid/asgi.py
+++ PlayGrid/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PlayGrid.settings')
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application() 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import ttt.routing
