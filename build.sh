2b3e9d9cd43a870c97a7a398406d082e98d52234
AnupBudhathoki <kshetria656@gmail.com>
2026-03-28 10:02:30 +0545

    buid reworked

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
+set -e
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-```
-
----
-
-**Step 2 — On Render set Build Command to:**
-```
-./build.sh
